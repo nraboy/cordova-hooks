b1fd95109caadfe9852347671888c318c9a3cba5
paishin <hpanic@gmail.com>
2015-01-18 19:52:07 +0200

    Changes to make the linting to work....sort of!
    In this version the hints are displayed fine BUT the process is not killed if errors are found since the command is commented out.
    If I try to enable it the process is killed before the comments are displayed!
    
    Following code was moved out of the function since it was never called
    /***
    if (filesWithErrorsCount > 0) {
        console.log("There were " + filesWithErrorsCount + " files with errors");
        //process.exit(1);
    }
    ***/

--- before_prepare/02_jshint.js
+++ before_prepare/02_jshint.js
@@ -13,26 +13,22 @@ var filesWithErrorsCount = 0;
 
 // All directories that contain JavaScript in the www path of your project you want linted
 var foldersToProcess = [
+    "js/controllers",
     "js"
 ];
 
-foldersToProcess.forEach(function(folder) {
+foldersToProcess.forEach(function (folder) {
     filesWithErrorsCount += processFiles("www/" + folder);
 });
-
-if(filesWithErrorsCount > 0) {
-    console.log("There were " + filesWithErrorsCount + " files with errors");
-    process.exit(1);
-}
 
 function processFiles(dir, callback) {
     var errorCount = 0;
     var fileList = fs.readdirSync(dir);
-    fileList.forEach(function(file) {
+    fileList.forEach(function (file) {
         file = dir + "/" + file;
         var fileStat = fs.statSync(file);
-        if(!fileStat.isDirectory()) {
-            if(path.extname(file) === ".js") {
+        if (!fileStat.isDirectory()) {
+            if (path.extname(file) === ".js") {
                 errorCount += lintFile(file);
             }
         }
@@ -43,18 +39,29 @@ function processFiles(dir, callback) {
 function lintFile(file) {
     console.log("Linting " + file);
     var fileData = fs.readFileSync(file);
-    if(jshint(fileData.toString())) {
+    if (jshint(fileData.toString())) {
         console.log("File " + file + " has no errors.");
         console.log("-----------------------------------------");
         return 0;
     } else {
         console.log("Errors in file " + file);
         var out = jshint.data(),
-        errors = out.errors;
-        for(var j = 0; j < errors.length; j++) {
+            errors = out.errors;
+
+        console.log('NUMBER OF ERRORS', errors.length)
+
+
+        for (var j = 1; j < errors.length; j++) {
+            if (errors[j] !== null)
             console.log(errors[j].line + ":" + errors[j].character + " -> " + errors[j].reason + " -> " + errors[j].evidence);
         }
+
         console.log("-----------------------------------------");
         return 1;
     }
+}
+
+if (filesWithErrorsCount > 0) {
+    console.log("There were " + filesWithErrorsCount + " files with errors");
+    //process.exit(1);
 }
